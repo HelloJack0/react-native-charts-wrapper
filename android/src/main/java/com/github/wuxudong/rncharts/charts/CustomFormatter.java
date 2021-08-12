a0a69b10437cf7dc33f9efe98384ac60bf43feb4
HelloJack0 <47816656+HelloJack0@users.noreply.github.com>
2021-08-12 21:08:41 +0800

    添加饼图自定义格式

--- android/src/main/java/com/github/wuxudong/rncharts/charts/CustomFormatter.java
+++ android/src/main/java/com/github/wuxudong/rncharts/charts/CustomFormatter.java
@@ -12,6 +12,19 @@ import java.text.DecimalFormat;
 public class CustomFormatter extends ValueFormatter {
 
     private DecimalFormat mFormat;
+    private boolean isPieChart = false;
+    private int sum = 0;
+    
+    public CustomFormatter(String value, String type) {
+        if (type.equals("PIE")) {
+            isPieChart = true;
+            String[] str = value.split("PIE");
+            sum = Integer.parseInt(str[1]);
+            mFormat = new DecimalFormat(str[0]);
+        } else {
+            mFormat = new DecimalFormat(value);
+        }
+    }
 
     public CustomFormatter(String value) {
         mFormat = new DecimalFormat(value);
